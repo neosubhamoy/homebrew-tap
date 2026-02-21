4b0b0e3cedefc5581ec8a4571c27f378423e1b7e
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2026-02-21 15:58:31 +0530

    feat: init homebrew tap

---
+++ Casks/neodlp.rb
@@ -0,0 +1,32 @@
+cask "neodlp" do
+    arch arm: "aarch64", intel: "x64"
+
+    version "0.4.1"
+    sha256 arm: "f3b10924b937d12886d3237ed22774277320361f7f72b05de3db944a8c4b4788",
+           intel: "be5a1c9341ee3d8c3541a8a4d8daf9bd380c7ee090427db4c57d2de86e8b55e1"
+
+    url "https://github.com/neosubhamoy/neodlp/releases/download/v#{version}/NeoDLP_#{version}_#{arch}.dmg",
+    verified: "github.com/neosubhamoy/neodlp/"
+    name "NeoDLP"
+    desc "Modern video/audio downloader based on yt-dlp with browser integration"
+    homepage "https://neodlp.neosubhamoy.com/"
+
+    livecheck do
+        url :url
+        strategy :github_latest
+    end
+
+    depends_on macos: ">= :big_sur"
+    depends_on formula: "aria2"
+
+    app "NeoDLP.app"
+
+    zap trash: [
+        "~/Library/LaunchAgents/com.neosubhamoy.neodlp.plist",
+        "~/Library/Application Support/Google/Chrome/NativeMessagingHosts/com.neosubhamoy.neodlp.json",
+        "~/Library/Application Support/Chromium/NativeMessagingHosts/com.neosubhamoy.neodlp.json",
+        "~/Library/Application Support/Mozilla/NativeMessagingHosts/com.neosubhamoy.neodlp.json",
+        "~/Library/Logs/com.neosubhamoy.neodlp",
+        "~/yt-dlp-plugins/bgutil-ytdlp-pot-provider"
+    ]
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Neosubhamoy Tap
+
+## How do I install these formulae?
+
+`brew install neosubhamoy/tap/<formula>`
+
+Or `brew tap neosubhamoy/tap` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "neosubhamoy/tap"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
